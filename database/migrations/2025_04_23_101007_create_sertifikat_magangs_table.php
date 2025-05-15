e0cfc0c3e35642f6c271058b9e5c230b2d7eaa2b
Halannn <hairulirawann@gmail.com>
2025-05-15 13:18:53 +0800

    fix(ui) : responsive at auth pages

--- database/migrations/2025_04_23_101007_create_sertifikat_magangs_table.php
+++ database/migrations/2025_04_23_101007_create_sertifikat_magangs_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Profile;
+use App\Models\ProfileMagang;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
